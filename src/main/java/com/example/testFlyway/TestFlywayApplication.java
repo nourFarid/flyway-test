4c54a4af446040fee68e8b776415424c5828046a
nourFarid <118819227+nourFarid@users.noreply.github.com>
2024-09-17 22:56:27 +0300

    first

---
+++ src/main/java/com/example/testFlyway/TestFlywayApplication.java
@@ -0,0 +1,13 @@
+package com.example.testFlyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestFlywayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestFlywayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testFlyway/TestFlywayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testFlyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestFlywayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
